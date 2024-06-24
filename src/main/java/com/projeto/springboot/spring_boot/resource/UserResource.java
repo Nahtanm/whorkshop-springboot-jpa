a47a3734cce89307dc13b5523bf78a899ca0204b
Nahtanm <nahtanmcs0019@gmail.com>
2024-06-24 14:23:57 -0300

    Service

--- src/main/java/com/projeto/springboot/spring_boot/resource/UserResource.java
+++ src/main/java/com/projeto/springboot/spring_boot/resource/UserResource.java
@@ -1,20 +1,40 @@
 package com.projeto.springboot.spring_boot.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.springboot.spring_boot.entities.User;
+import com.projeto.springboot.spring_boot.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User();
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> list = userService.findAll();
+		
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User user = userService.findById(id);
+		
 		return ResponseEntity.ok().body(user);
+		
 	}
 	
 }
