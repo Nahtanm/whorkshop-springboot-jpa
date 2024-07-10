c0b5dc1b9e2fb973b510bf14df48afc258475fc4
Nahtanm <nahtanmcs0019@gmail.com>
2024-07-10 12:54:36 -0300

    OrderItem

---
+++ src/main/java/com/projeto/springboot/spring_boot/entities/OrderItem.java
@@ -0,0 +1,87 @@
+package com.projeto.springboot.spring_boot.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.projeto.springboot.spring_boot.entities.pk.OrderIntemPk;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private OrderIntemPk id;
+
+	private Integer quantidade;
+	private double preco;
+	
+	public OrderItem(Order order, Product product, Integer quantidade, double preco) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+
+	public OrderItem() {
+		super();
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
