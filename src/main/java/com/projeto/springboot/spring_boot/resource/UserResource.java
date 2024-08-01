3157324669519ee44197afac2a6efdf2d5ab343d
Nahtanm <nahtanmcs0019@gmail.com>
2024-08-01 14:01:25 -0300

    Update

--- src/main/java/com/projeto/springboot/spring_boot/resource/UserResource.java
+++ src/main/java/com/projeto/springboot/spring_boot/resource/UserResource.java
@@ -27,22 +27,29 @@ public class UserResource {
 		
 	}
 	
-	@GetMapping(value="/{id}")
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id){
-		
 		User user = userService.findById(id);
-		
 		return ResponseEntity.ok().body(user);
-		
 	}
 
 	@PostMapping
 	public ResponseEntity<User> salvarUser(@RequestBody User user){
-
 		user = userService.inserir(user);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(user.getId()).toUri();
 		return ResponseEntity.created(uri).body(user);
+	}
 
+	@DeleteMapping (value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping (value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+		user = userService.update(id,user);
+		return ResponseEntity.ok().body(user);
 	}
 	
 }
--- src/main/java/com/projeto/springboot/spring_boot/services/UserService.java
+++ src/main/java/com/projeto/springboot/spring_boot/services/UserService.java
@@ -16,23 +16,32 @@ public class UserService {
 	private UserRepository userRepository;
 
 	public List<User> findAll(){
-		
 		return userRepository.findAll();
-		
 	}
 	
 	public User findById(Long id) {
-		
 		Optional<User> user = userRepository.findById(id);
-		
 		return user.get();
-		
 	}
 
 	public User inserir(User user){
-
 		return userRepository.save(user);
-
 	}
-	
+
+	public void delete(Long id){
+		userRepository.deleteById(id);
+	}
+
+	public User update(Long id, User user){
+		User user_1 = userRepository.getReferenceById(id);
+		updateData(user_1, user);
+		return userRepository.save(user_1);
+	}
+
+	public void updateData(User user_1, User user){
+		user_1.setName(user.getName());
+		user_1.setEmail(user.getEmail());
+		user_1.setPhone(user.getPhone());
+	}
+
 }
