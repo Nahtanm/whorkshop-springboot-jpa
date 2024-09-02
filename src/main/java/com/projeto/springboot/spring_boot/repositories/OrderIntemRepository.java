ba094e7fca5498a98d46ab405e00534124c06257
Nahtanm <nahtanmcs0019@gmail.com>
2024-09-02 14:38:03 -0300

    Nhé

--- src/main/java/com/projeto/springboot/spring_boot/repositories/OrderIntemRepository.java
+++ src/main/java/com/projeto/springboot/spring_boot/repositories/OrderIntemRepository.java
@@ -4,5 +4,5 @@ import com.projeto.springboot.spring_boot.entities.OrderItem;
 import com.projeto.springboot.spring_boot.entities.pk.OrderIntemPk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderIntemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderIntemRepository extends JpaRepository<OrderItem, OrderIntemPk> {
 }
--- src/main/java/com/projeto/springboot/spring_boot/resource/UserResource.java
+++ src/main/java/com/projeto/springboot/spring_boot/resource/UserResource.java
@@ -12,44 +12,45 @@ import com.projeto.springboot.spring_boot.services.UserService;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		
+	public ResponseEntity<List<User>> findAll() {
+
 		List<User> list = userService.findAll();
-		
+
 		return ResponseEntity.ok().body(list);
-		
+
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 
 	@PostMapping
-	public ResponseEntity<User> salvarUser(@RequestBody User user){
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
 		user = userService.inserir(user);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(user.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(user.getId())
+				.toUri();
 		return ResponseEntity.created(uri).body(user);
 	}
 
-	@DeleteMapping (value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id){
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 
-	@PutMapping (value = "/{id}")
-	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
-		user = userService.update(id,user);
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+		user = userService.update(id, user);
 		return ResponseEntity.ok().body(user);
 	}
-	
+
 }
